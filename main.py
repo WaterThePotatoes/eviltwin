f9eecc27e7e758af73076479e115680a330a7f34
Bianca <130195861+bianca-8@users.noreply.github.com>
2024-11-23 16:05:27 -0500

    kirby!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-import time
+from player import Player
 
 pygame.init()
 pygame.font.init()
@@ -8,24 +8,6 @@ screenW,screenH = 800, 600
 screen = pygame.display.set_mode((screenW,screenH))
 clock = pygame.time.Clock()
 end = False
-
-class Character(object):
-    def __init__(self, image, width, height, pos):
-        self.image = pygame.image.load(image)
-        self.width = width
-        self.height = height
-        self.x,self.y = pos
-
-    def draw(self):
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        screen.blit(self.image, (self.x, self.y))
-
-    def Rect(self):
-        return pygame.Rect(self.x,self.y,self.width,self.height)
-
-class CurrentCharacter(Character):
-    def __init__(self, image, width, height, pos):
-        self.image = pygame.image.load(image)
 
 over_font = pygame.font.SysFont('Comic Sans MS', 80)
 
@@ -40,7 +22,7 @@ while mainloop:
             mainloop = False
             exit()
         while end:
-            screen.fill((0,0,0))
+            screen.fill((255,0,0))
             game_over = over_font.render("Game Over", False, (255, 255, 255))
             screen.blit(game_over, (100, 150))
             pygame.display.update()
@@ -49,57 +31,3 @@ while mainloop:
                     mainloop = False
                     exit()
                 break
-
-"""
-class CurrentCharacter(Character):
-    def __init__(self,name,image,width,height,pos,speed):
-        self.name = name
-        self.image = pg.image.load(image)
-        self.images = [self.image]
-        self.width = width
-        self.height = height
-        self.x,self.y = pos
-        self.speed = speed
-        self.rect = self.image.get_rect()
-        self.th = self.height
-        self.tw = self.width
-
-    def Rect(self):
-        self.rect = pg.Rect(self.x,self.y,self.tw,self.th)
-        return self.rect
-
-    def run(self):
-        self.rect = self.Rect()
-        if keys[pg.K_LEFT] and self.x > 0+self.width:
-            self.x -= self.speed
-            for i in self.images[1:]:
-                i.x -= self.speed
-
-
-        if keys[pg.K_RIGHT] and self.x < screenW - self.width:
-            self.x += self.speed
-            for i in self.images[1:]:
-                i.x += self.speed
-
-        if keys[pg.K_UP] and self.y > 0+self.height:
-            self.y -= self.speed
-            for i in self.images[1:]:
-                i.y -= self.speed
-
-        if keys[pg.K_DOWN] and self.y < screenH - self.height:
-            self.y += self.speed
-            for i in self.images[1:]:
-                i.y += self.speed
-
-
-        for x in collidables:
-            if self.rect.colliderect(x.Rect()):
-                self.images.append(x)
-
-                self.x = min(self.x, x.x)
-                self.y = min(self.y, x.y)
-                self.tw = max(self.x + self.width, x.x + x.width) - self.x
-                self.th = max(self.y + self.height, x.y + x.height) - self.y
-
-
-                collidables.remove(x)"""
